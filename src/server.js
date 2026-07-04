7047de569afb50e00d95f74ec5e3ddf55c5a62e9
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-07-04 18:27:28 +0300

    connect with mangodb

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
 
 const app = express();
 
@@ -62,6 +63,8 @@ app.use((err, req, res, next) => {
       : err.message,
   });
 });
+
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
