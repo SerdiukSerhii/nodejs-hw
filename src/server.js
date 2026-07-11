d006a3a8fd5c755c146ac5f07812f73ad3033590
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-07-11 18:11:55 +0300

    add validation for rout notes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -22,6 +23,8 @@ app.use(
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+
+app.use(errors());
 
 app.use(errorHandler);
 
